1e74b6aba59b8c46142c51355ad41ccdbdde956e
GUANTAI001 <jpguantai@gmail.com>
2023-05-28 19:10:29 +0300

    inial

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,18 +1,72 @@
 import { BellFilled, MailOutlined } from "@ant-design/icons";
-import { Badge, Drawer, Image, List, Space, Typography } from "antd";
-import { useEffect, useState } from "react";
+import { Badge, Drawer, Image, List, Space } from "antd";
+import {  useState } from "react";
 import {  } from "../API/data";
 import Profile from "../images/download.jpeg";
 
-type Notification = {}
+
 
 const NavHeader = () => {
+
+    const [commentsOpen, setCommentsOpen] = useState<boolean>(false);
+    const [notificationsOpen, setNotificationsOpen] = useState<boolean>(false);
+
+
+
     return (
-        <>
-        </>
+        <div className="flex flex-row bg-white fixed   justify-evenly w-full p-2 ">
+        <h1 className="text-xl mt-2 font-bold">Guantai Dashboad</h1>
+        <div className="ml-12 ">
+          <Space>
+            <Badge className="ml-2" 
+            >
+              <MailOutlined
+                style={{ fontSize: 24 }}
+                onClick={() => {
+                  setCommentsOpen(true);
+                }}
+              />
+            </Badge>
+            <Badge className="ml-2" 
+         
+            >
+              <BellFilled
+                style={{ fontSize: 24 }}
+                onClick={() => {
+                   setNotificationsOpen(true);
+                }}
+              />
+            </Badge>
+            <Image className="ml-2" width={40} src={Profile} ></Image>
+          </Space>
+        </div>
+  
+        <Drawer
+          title="Comments"
+          open={commentsOpen}
+          onClose={() => {
+            setCommentsOpen(false);
+          }}
+          maskClosable
+        >
+          <List
+         //Data will be fetched from the API
+          ></List>
+        </Drawer>
+        <Drawer
+          title="Notifications"
+          open={notificationsOpen}
+          onClose={() => {
+            setNotificationsOpen(false);
+          }}
+          maskClosable
+        >
+          <List
+         //Data will be fetched from the API
+          ></List>
+        </Drawer>
+      </div>
     );
-
-};
-
-export default NavHeader;
-
+  }
+  export default NavHeader;
+  
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,6 @@ import {
 } from "@ant-design/icons";
 import { Card, Space, Statistic, Table, } from "antd";
 import { useEffect, useState } from "react";
-import { data } from "../API/data";
 
 import {
     Chart as ChartJS,
@@ -39,6 +38,17 @@ interface OrderProps {
     total: number;
 }
 
+interface RevenueData {
+    labels: string[];
+    datasets: {
+        label: string;
+        data: number[];
+        backgroundColor: string;
+    }[];
+}
+
+
+
 interface ApiResponse {
     products: OrderProps[];
 }
@@ -46,13 +56,15 @@ interface ApiResponse {
 const Dashboard = () => {
 
     const [loading, setLoading] = useState<boolean>(true);
-    const [orders, setOrders] = useState<number>(0); 
+    const [orders, setOrders] = useState<number>(0);
     const [inventory, setInventory] = useState<number>(0);
     const [customers, setCustomers] = useState<number>(0);
     const [revenue, setRevenue] = useState<number>(0);
-    const [recent , setRecent] = useState<OrderProps[]>([]);
-    const [revenueData, setRevenueData] = useState<number[]>([]);
-
+    const [recent, setRecent] = useState<OrderProps[]>([]);
+    const [revenueData, setRevenueData] = useState<RevenueData>({
+        labels: [],
+        datasets: [],
+    });
 
     useEffect(() => {
         setLoading(true);
@@ -80,15 +92,45 @@ const Dashboard = () => {
             });
 
         fetch("https://dummyjson.com/carts")
-            .then((response) => response.json())    
-            .then((data) => {
-                setRevenueData(data.data);
-            }
-            );
+            .then((response) => response.json())
+            .then((res) => {
+                const labels = res.carts.map((cart: any) => {
+                    return `User ${cart.userId}`;
 
+                });
+                const data = res.carts.map((cart: any) => {
+                    return cart.discountedTotal;
+                });
+                const dataSource = {
+                    labels,
+                    datasets: [
+                        {
+                            label: "Revenue",
+                            data: data,
+                            backgroundColor: "rgba(255, 0, 0, 1)",
+                        },
+                    ],
+                };
+                setRevenueData(dataSource);
+
+            })
     }, []);
 
- 
+    const option = {
+        responsive: true,
+        Plugins: {
+            legend: {
+                position: "bottom",
+            },
+            title: {
+                display: true,
+                text: "Order Revenue",
+            },
+        },
+    }
+        
+
+
 
 
 
@@ -148,11 +190,11 @@ const Dashboard = () => {
                     <Space direction="horizontal">
                         <UserOutlined
                             style={{
-                                    color: "purple",
-                                    backgroundColor: "rgba(0,255,255,0.25)",
-                                    borderRadius: 20,
-                                    fontSize: 24,
-                                    padding: 8,
+                                color: "purple",
+                                backgroundColor: "rgba(0,255,255,0.25)",
+                                borderRadius: 20,
+                                fontSize: 24,
+                                padding: 8,
                             }}
                         />
                         <Statistic
@@ -183,9 +225,10 @@ const Dashboard = () => {
                 </Card>
             </Space>
 
-            <div className="flex flex-col justify-center mt-8">
+            <Space direction="horizontal">
+            <div className="flex flex-col justify-center">
                 <h1 className="text-xl mt-8 font-bold">Recent Orders</h1>
-                <Table 
+                <Table
                     loading={loading}
                     columns={[
                         {
@@ -200,13 +243,22 @@ const Dashboard = () => {
                             title: "Price",
                             dataIndex: "discountedPrice",
                         },
-                        
+
                     ]}
                     dataSource={recent}
                     pagination={false}
                 > </Table>
 
-        </div>
+            </div>
+            <div className="flex flex-col mt-8 justify-center">
+                <Card style={{ width: 500, height: 250 }}>
+                    <Bar 
+                    options={option}
+                    data={revenueData} 
+                    />
+                </Card>
+            </div>
+            </Space>
         </div>
     );
 };
